9a311a6e0e92388684275184cc42985a3cb28837
PRIYANKA CHAUDHARI <117078620+1PRIYANKA1@users.noreply.github.com>
2022-11-08 12:52:19 +0530

    Add files via upload

---
+++ Triangle.py
@@ -0,0 +1,4 @@
+b=6
+h=8
+a=0.5*b*h;
+print("area of triangle is ",a)
---
+++ pythonString.py
@@ -0,0 +1,8 @@
+str= "Hello bye"
+print(str)
+print(str [0])
+print (str [2:5])
+print (str [2: ])
+print (str [ :5])
+print (str+"hii")
+print(str*4)

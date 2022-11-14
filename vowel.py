add9b6aace4e72ae8e4d0bf990445ad5123e0eda
PRIYANKA CHAUDHARI <117078620+1PRIYANKA1@users.noreply.github.com>
2022-11-14 18:39:52 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,6 @@
+#Accept a lowercase character from the user and check whether the character is a vowel or consonant.
+ch=input("enter a character: ")
+if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u':
+    print("vowel")
+else:
+    print("consonant")
